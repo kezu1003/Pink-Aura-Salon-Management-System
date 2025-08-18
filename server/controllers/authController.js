0eca17e4011dfec55d389c0ad3fcdf8fa8a736ea
Praveen Dinuwara <dinuwara01@gmail.com>
2025-08-18 11:41:26 +0530

    Update AuthContoller
    +registration
    +login
    +logging out

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,7 +23,7 @@ export const register = async (req,res)=>{
 
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn: '7d'});
 
-        register.cookie('token', token, {
+        res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ?
@@ -31,7 +31,64 @@ export const register = async (req,res)=>{
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
+         return res.json({success: true});
+
     }catch (error){
         res.json({success: false, message: error.message})
+    }
+}
+export const login = async (req,res)=> {
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.json({success: false, message: 'email and password are required'})
+
+    }
+
+    try{
+
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success:false, message: 'Invalid email'})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        
+        if(!isMatch){
+            return res.json({success:false, message: 'Invalid password'})
+        }
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn: '7d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ?
+            'none': 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({success: true});
+
+
+
+    }catch(error){
+        return res.json({success: false, message: error.message});
+    }
+}
+
+export const logout = async (req,res)=>{
+    try{
+        res.clearCookie('token',{
+            httpOnly: true,
+            secure:process.envNODE_ENV == 'production',
+            sameSite: process.env.NODE_ENV === 'production'?
+            'none':'strict',
+        })
+
+        return res.json({success: true, message:"logged out"})
+
+    }catch(error){
+        return res.json({ success: false, message: error.message});
     }
 }
