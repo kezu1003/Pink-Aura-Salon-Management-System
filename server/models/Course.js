036a09332f2d2b29ab566419024403cde1aabcec
kezu1003 <keshani2001karunarathne@gmail.com>
2025-09-21 15:20:24 +0530

    added course model file

---
+++ server/models/Course.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { courseConnection } from "../config/db.js"; // import the correct connection
+
+const courseSchema = new mongoose.Schema(
+{
+    courseName: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: String,
+        required: true,
+    },
+    instructorName: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    schedule: {
+        type: String,
+        required: true,
+    }
+},
+{ timestamps: true }
+);
+
+const Course = courseConnection.model("Course", courseSchema); // use courseConnection
+
+export default Course;
