7d607d1e327d1467bd1b332f79e205bd62f0bb69
shanalie26 <shanalie01@outlook.com>
2025-10-01 11:35:28 +0530

    update service.jsx
    background image fixed to top

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -55,7 +55,7 @@ export default function Services() {
 
       {/* background image */}
       <section
-        className="relative h-[60vh] flex items-center justify-center bg-cover bg-center"
+        className="relative h-[60vh] flex items-center justify-center bg-cover bg-top"
         style={{ backgroundImage: "url('/serv01.png')" }}
       >
         <div className="absolute inset-0 bg-black/40" /> {/* dark overlay */}
